09dc85d5d6a2bc7719984b43a32eb88ef45bbdc5
amol <amoljandial41@gmail.com>
2026-05-03 13:08:44 +0530

    added skills section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "997552784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3117050840" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
